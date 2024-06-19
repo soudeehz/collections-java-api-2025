e93e4b6acc31b323aea325aafd6de9a62a092983
Denzel Jhones <denzelxd09@icloud.com>
2024-06-18 21:40:48 -0300

    Tentando resolver desafio tarefa
    Signed-off-by: Denzel Jhones <denzelxd09@icloud.com>

---
+++ src/OperacoesBasicas/Tarefas.java
@@ -0,0 +1,14 @@
+package OperacoesBasicas;
+
+public class Tarefas {
+    public Tarefas(String descricao) {
+        this.descricao = descricao;
+    }
+
+    // atributo
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
